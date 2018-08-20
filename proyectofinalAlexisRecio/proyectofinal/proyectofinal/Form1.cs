72022ea9f31ff68511b868093ee5f7f8653791e9
Alexis Recio <40526704+Alexisrecio@users.noreply.github.com>
2018-08-19 22:58:13 -0400

    subida inicial del proyecto

---
+++ proyectofinalAlexisRecio/proyectofinal/proyectofinal/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectofinal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double radio;
+        double area;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            radio = Convert.ToDouble(textBox1.Text);
+            area = Math.PI * radio * radio;
+            label2.Text = Convert.ToString("El area del circulo es:" + area); 
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
